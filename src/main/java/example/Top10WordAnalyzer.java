9f364a84553dc7c4b79cdfbfae7e7207fa52e84a
nik <nikitap4.92@gmail.com>
2017-12-28 13:14:28 +0300

    Top 10 words spark application example

---
+++ src/main/java/example/Top10WordAnalyzer.java
@@ -0,0 +1,28 @@
+package example;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
+
+import java.util.Arrays;
+
+public final class Top10WordAnalyzer {
+
+    public static void main(String[] args)  {
+        JavaSparkContext sc = new JavaSparkContext(new SparkConf().setAppName("Top10"));
+
+        sc.textFile(args[0]).map(line -> line.replaceAll("[^A-Za-z]+", " "))
+                .flatMap(line -> Arrays.asList(line.split("\\s+")).iterator())
+                .map(String::toLowerCase)
+                .mapToPair(word -> new Tuple2<>(word, 1L))
+                .reduceByKey((x, y) ->  x + y)
+                .collect()
+                .stream()
+                .sorted((t1, t2) -> t2._2.compareTo(t1._2))
+                .map(tuple -> tuple._1() + ": " + tuple._2())
+                .limit(10)
+                .forEach(System.out::println);
+
+        sc.stop();
+    }
+}
